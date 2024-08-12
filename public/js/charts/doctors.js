9398cfbf6a888a49859d5f480616ee05419a84eb
Alexiotovv <gpsalexvasquez@gmail.com>
2024-08-12 00:00:45 -0500

    resend_email

--- public/js/charts/doctors.js
+++ public/js/charts/doctors.js
@@ -1,24 +1,25 @@
-const chart = Highcharts.chart('container', {
+const chart =
+Highcharts.chart('container', {
     chart: {
         type: 'column'
     },
     title: {
-        text: 'Desempeño médico'
+        text: 'Desempeño de los Doctores'
     },
     xAxis: {
         categories: [],
         crosshair: true
     },
     yAxis: {
+        min: 0,
         title: {
-            useHTML: true,
-            text: 'Citas atendidas'
+            text: 'Citas Atendidas'
         }
     },
     tooltip: {
         headerFormat: '<span style="font-size:10px">{point.key}</span><table>',
         pointFormat: '<tr><td style="color:{series.color};padding:0">{series.name}: </td>' +
-            '<td style="padding:0"><b>{point.y:.1f}</b></td></tr>',
+            '<td style="padding:0"><b>{point.y:.1f} mm</b></td></tr>',
         footerFormat: '</table>',
         shared: true,
         useHTML: true
@@ -32,37 +33,37 @@ const chart = Highcharts.chart('container', {
     series: []
 });
 
+
 let $start, $end;
-
-function fetchData(){
-    const startDate = $start.val();
-    const endDate = $end.val();
-    const url = `/reportes/doctors/column/data?start=${startDate}&end=${endDate}`;
-
+function fetchData()
+{
+    const $startDate = $start.val();
+    const $endDate = $end.val();
+    const $url = `/reportes/doctors/column/data?start=${$startDate}&end=${$endDate}`;
     //Fetch API
-    fetch(url)
-        .then(function(response) {
-            return response.json();
-        })
-        .then(function(myJson) {
-            //console.log(myJson);
-            chart.xAxis[0].setCategories(myJson.categories);
+    fetch($url)
+    .then(function (response) {
+        return response.json();
+    })
+    .then(function (myJason) {
+        //console.log(data);
+        chart.xAxis[0].setCategories(myJason.categories);
 
-            if(chart.series.length > 0){
-                chart.series[1].remove();
-                chart.series[0].remove();
-            }
+        if(chart.series.length > 0)
+        {
+            chart.series[1].remove();
+            chart.series[0].remove();
+        }
 
-            chart.addSeries(myJson.series[0]); //Citas Atendidas
-            chart.addSeries(myJson.series[1]); //Citas Canceladas
-        });
+        chart.addSeries(myJason.series[0]);//Citas Atendidas
+        chart.addSeries(myJason.series[1]);//Citas Canceladas
+
+    })
 }
 
-$(function (){
-
+$(function(){
     $start = $('#startDate');
     $end = $('#endDate');
-
     fetchData();
 
     $start.change(fetchData);
